4f4f62fcddb17dda2936c2adfe128e8b97e68b94
Will Hindenburg <WillH1983@yahoo.com>
2015-01-30 20:17:46 -0600

    Initial Commit

---
+++ BudgetTracker/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BudgetTracker
+//
+//  Created by William Hindenburg on 1/30/15.
+//  Copyright (c) 2015 William Hindenburg. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
