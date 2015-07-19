c663b30b22fc00daef9082fce845bd57854e7c8e
Will Hindenburg <WillH1983@yahoo.com>
2015-07-19 01:18:00 -0500

    Updated to use some better swift code.

--- BudgetTracker/Services/RetrievePurchasesService.swift
+++ BudgetTracker/Services/RetrievePurchasesService.swift
@@ -12,67 +12,50 @@ import CloudKit
 class RetrievePurchasesService: NSObject {
     func retrievePurchases (completionHandler:((Array:[AnyObject]) -> Void), errorHandler:((NSError) -> Void)) {
         let cloudDatabase = CKContainer.defaultContainer().publicCloudDatabase
-        let predicate = NSPredicate(value:true)
+        let predicate = NSPredicate(value: true)
         let query = CKQuery(recordType: "Purchase", predicate: predicate)
         
-        cloudDatabase.performQuery(query, inZoneWithID: nil, completionHandler: { (arrayOfPurchases, error) -> Void in
+        cloudDatabase.performQuery(query, inZoneWithID: nil, completionHandler: { (arrayOfRecords, error) -> Void in
             dispatch_async(dispatch_get_main_queue(),{
                 if error == nil {
-                    let mutableArrayOfPurchaseMonths = NSMutableArray()
-                    var purchaseArray = [Purchase]()
-                    var arrayOfPurchases2 = []
-                    if (arrayOfPurchases != nil) {
-                        arrayOfPurchases2 = arrayOfPurchases!
-                    }
-                    for record in arrayOfPurchases2 {
-                        if let actualRecord = record as? CKRecord {
-                            let purchase = Purchase()
-                            purchase.purchaseAmount = actualRecord.objectForKey("PurchaseAmount") as! Int
-                            purchase.purchasePlace = actualRecord.objectForKey("PurchasePlace") as! String
-                            if let variablePurchaseDate: NSDate = actualRecord.objectForKey("PurchaseDate") as? NSDate {
-                                purchase.purchaseDate = variablePurchaseDate
-                            }
-                            
-                            let beginningOfTheMonthDate = self.dateAtTheBeginningOfTheMonth(purchase.purchaseDate)
-                            
-                            var monthDictionary = NSDictionary()
+                    /* Array of dictionaries, each dictionary has two keys, one has key of "month" which is an NSDate.
+                    The second is "purchases" which contains Array of "Purchases"*/
+                    var arrayOfMonths = [Dictionary<String, AnyObject>]()
+                    let arrayOfPurchases = arrayOfRecords ?? [CKRecord]()
+                    
+                    for purchase in arrayOfPurchases {
+                        
+                        /* If there are no months created, then there are none to search for,
+                        add the month for the current purchase. */
+                        if arrayOfMonths.count > 0 {
+                            /* Search for the month for the purchase, and add it to the dictionary */
                             var foundMatch = false
-                            if mutableArrayOfPurchaseMonths.count > 0 {
-                                for object in mutableArrayOfPurchaseMonths {
-                                    monthDictionary = object as! NSDictionary
-                                    if let monthDate = monthDictionary.valueForKey("month") as? NSDate {
-                                        if (monthDate.isEqualToDate(beginningOfTheMonthDate)) {
-                                            if let arrayOfPurchases2 = monthDictionary.valueForKey("purchases") as? NSMutableArray {
-                                                arrayOfPurchases2.addObject(purchase)
-                                            }
-                                            foundMatch = true
-                                            continue
-                                        }
-                                    }
+                            for var index = 0; index < arrayOfMonths.count; ++index {
+                                var purchaseDictionary = arrayOfMonths[index]
+                                let month = purchaseDictionary["month"] as! NSDate
+                                let purchaseDate = self.purchaseDateFromRecord(purchase)
+                                if month == self.dataAtTheBeginningOfTheMonthFromDate(purchaseDate) {
+                                    var purchaseArray = purchaseDictionary["purchases"] as! Array<Purchase>
+                                    purchaseArray.append(self.createPurchaseFromRecord(purchase))
+                                    purchaseDictionary["purchases"] = purchaseArray
+                                    arrayOfMonths[index] = purchaseDictionary
+                                    foundMatch = true
+                                    break
                                 }
-                                
-                                if foundMatch == false {
-                                    let mutablePurchaseArray = NSMutableArray()
-                                    mutablePurchaseArray.addObject(purchase)
-                                    mutableArrayOfPurchaseMonths.addObject(["month":beginningOfTheMonthDate, "purchases":mutablePurchaseArray])
-                                }
-                            } else {
-                                let mutablePurchaseArray = NSMutableArray()
-                                mutablePurchaseArray.addObject(purchase)
-                                mutableArrayOfPurchaseMonths.addObject(["month":beginningOfTheMonthDate, "purchases":mutablePurchaseArray])
                             }
+                            if (foundMatch == false) {
+                                arrayOfMonths.append(self.topLevelPurchaseDictionaryFromRecord(purchase))
+                            }
+                        } else {
+                            /* Create the first month dictionary in the array */
+                            arrayOfMonths.append(self.topLevelPurchaseDictionaryFromRecord(purchase))
                             
-                            
-                            
-                            
-                            purchaseArray.append(purchase)
                         }
                     }
                     let dateSortDescriptiors = NSSortDescriptor(key: "month", ascending: false)
-                    let arrayOfDictionaries = mutableArrayOfPurchaseMonths.sortedArrayUsingDescriptors([dateSortDescriptiors])
-                    completionHandler(Array: arrayOfDictionaries);
-                } else {
-                    errorHandler(error!)
+                    let newArrayOfMonths = arrayOfMonths as NSArray
+                    let arrayOfDictionaries = newArrayOfMonths.sortedArrayUsingDescriptors([dateSortDescriptiors])
+                    completionHandler(Array: arrayOfDictionaries)
                 }
             })
             
@@ -80,7 +63,19 @@ class RetrievePurchasesService: NSObject {
         
     }
     
-    private func dateAtTheBeginningOfTheMonth (date: NSDate) -> NSDate {
+    private func topLevelPurchaseDictionaryFromRecord (record: CKRecord) -> [String: AnyObject] {
+        let purchaseArray: Array <Purchase> = [self.createPurchaseFromRecord(record)]
+        var purchaseDictionary = [String: AnyObject]()
+        purchaseDictionary = ["purchases": purchaseArray]
+        purchaseDictionary.updateValue(self.dateAtTheBeginningOfTheMonthFromRecord(record), forKey: "month")
+        return purchaseDictionary
+    }
+    
+    private func purchaseDateFromRecord (record: CKRecord) -> NSDate {
+        return record.objectForKey("PurchaseDate") as? NSDate ?? NSDate()
+    }
+    
+    private func dataAtTheBeginningOfTheMonthFromDate (date: NSDate) -> NSDate {
         let calendar = NSCalendar.currentCalendar()
         let timeZone = NSTimeZone.systemTimeZone()
         calendar.timeZone = timeZone
@@ -93,5 +88,18 @@ class RetrievePurchasesService: NSObject {
         
         let beginningOfMonth = calendar.dateFromComponents(dateComps)
         return beginningOfMonth ?? NSDate()
+    }
+    
+    private func createPurchaseFromRecord (record: CKRecord) -> Purchase {
+        let purchase = Purchase()
+        purchase.purchaseAmount = record.objectForKey("PurchaseAmount") as! Int
+        purchase.purchasePlace = record.objectForKey("PurchasePlace") as! String
+        purchase.purchaseDate = self.purchaseDateFromRecord(record)
+        return purchase
+    }
+    
+    private func dateAtTheBeginningOfTheMonthFromRecord (record: CKRecord) -> NSDate {
+        let date = self.purchaseDateFromRecord(record)
+        return self.dataAtTheBeginningOfTheMonthFromDate(date)
     }
 }
